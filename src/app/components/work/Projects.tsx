fb4d164c09b394211fed759f446fd0ce8d19f62e
SharanKarchella <sharankarchella@gmail.com>
2024-10-15 12:23:48 +0530

    Added Disney+Hotstar Project
    Added Disney+Hotstar Project

--- src/app/components/work/Projects.tsx
+++ src/app/components/work/Projects.tsx
@@ -4,9 +4,9 @@ import Project from './Project'
 function Projects() {
   return (
     <div className='grid grid-cols-3 gap-4'>
+      <Project link="https://github.com/SharanKarchella/Disney-Hotstar" title="Disney-Hotstar-Website" src="dh.png"/>
         <Project link="https://github.com/SharanKarchella/git-prof-finder" title="git-profile-finder" src="git.png"/>
         <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car.png"/>
-        <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car2.png"/>
         <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car3.png"/>
     </div>
   )
