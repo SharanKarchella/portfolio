d266226b5ae9f544ff4e64778b2c9a3cb5e97d67
SharanKarchella <sharankarchella@gmail.com>
2024-12-12 15:38:26 +0530

    new2

--- src/app/components/work/Projects.tsx
+++ src/app/components/work/Projects.tsx
@@ -4,7 +4,7 @@ import Project from './Project'
 function Projects() {
   return (
     <div className='grid grid-cols-3 gap-4'>
-      
+      <Project link="https://github.com/SharanKarchella/blog" title="Medium-Website-Blog" src="medium.png" />
       <Project link="https://github.com/SharanKarchella/Disney-Hotstar" title="Disney-Hotstar-Website" src="dh.png" />
       <Project link="https://github.com/SharanKarchella/git-prof-finder" title="git-profile-finder" src="git.png" />
       <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car.png" />
