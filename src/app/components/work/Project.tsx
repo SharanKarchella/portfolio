d713afc33abea428cacddbc625f70627fd4be3db
SharanKarchella <sharankarchella@gmail.com>
2024-12-12 15:10:38 +0530

    updated_button_livesite

--- src/app/components/work/Project.tsx
+++ src/app/components/work/Project.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import LiveSite from "./LiveSite";
 
 function Project(props: any) {
   return (
-    <a href={props.link} className="h-60 bg-cyan-600 p-4 rounded-xl bg-cover bg-right" style={{backgroundImage:`url("/images/work/${props.src}")`}}>
-      <span className="bg-white p-2 rounded-lg">{props.title}</span>
-    </a>
+    <div>
+      <a href={props.link} className="h-60 bg-cyan-600 p-4 rounded-xl bg-cover bg-right" style={{ backgroundImage: `url("/images/work/${props.src}")` }}>
+        <span className="bg-white p-2 rounded-lg">{props.title}</span>
+      </a>
+      <LiveSite href={props.liveSite} />
+    </div>
   );
 }
 export default Project;
