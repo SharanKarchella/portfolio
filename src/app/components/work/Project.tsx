58f5100dd0b4088893ac339b8b0b0b55dc96e896
SharanKarchella <sharankarchella@gmail.com>
2024-12-12 16:37:27 +0530

    full_new_update

--- src/app/components/work/Project.tsx
+++ src/app/components/work/Project.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 
-function Project(props: any) {
+function Project(props: any, target?: string) {
   return (
-      <a href={props.link} className="h-60 bg-cyan-600 p-4 rounded-xl bg-cover bg-right" style={{ backgroundImage: `url("/images/work/${props.src}")` }}>
+      <a href={props.link} className="h-60 bg-cyan-600 p-4 rounded-xl bg-cover bg-right" target={props.target || "_self"}  style={{ backgroundImage: `url("/images/work/${props.src}")` }}>
         <span className="bg-white p-2 rounded-lg">{props.title}</span>
       </a>
   );
--- src/app/components/work/Projects.tsx
+++ src/app/components/work/Projects.tsx
@@ -4,12 +4,12 @@ import Project from './Project'
 function Projects() {
   return (
     <div className='grid grid-cols-3 gap-4'>
-      <Project link="https://blog-bice-rho-26.vercel.app/signup" title="Medium-Website-Blog" src="medium.png" />
-      <Project link="https://movie-stream-movies.netlify.app/" title="Disney-Hotstar-Website" src="dh.png" />
-      <Project link="https://my-portfolio-nextjs-tailwind.netlify.app/" title="Portfolio-Website" src="portfolio.png" />
-      <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car.png" />
-      <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car3.png" />
-      <Project link="https://github.com/SharanKarchella/git-prof-finder" title="git-profile-finder" src="git.png" />
+      <Project link="https://blog-bice-rho-26.vercel.app/signup" title="Medium-Website-Blog" src="medium.png" target="_blank" />
+      <Project link="https://movie-stream-movies.netlify.app/" title="Disney-Hotstar-Website" src="dh.png" target="_blank" />
+      <Project link="https://my-portfolio-nextjs-tailwind.netlify.app/" title="Portfolio-Website" src="portfolio.png" target="_blank" />
+      <Project link="https://car-website-react-js.netlify.app/" title="Car-Website" src="car.png" target="_blank" />
+      <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car3.png" target="_blank" />
+      <Project link="https://github.com/SharanKarchella/git-prof-finder" title="git-profile-finder" src="git.png" target="_blank" />
 
 
     </div>
