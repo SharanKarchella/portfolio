d85b5ff598de8a8fa980f3f0b4bd0daae998be61
SharanKarchella <sharankarchella@gmail.com>
2024-12-12 15:24:54 +0530

    new1

--- src/app/components/work/Project.tsx
+++ src/app/components/work/Project.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import LiveSite from "./LiveSite";
+
 
 function Project(props: any) {
   return (
@@ -7,7 +7,6 @@ function Project(props: any) {
       <a href={props.link} className="h-60 bg-cyan-600 p-4 rounded-xl bg-cover bg-right" style={{ backgroundImage: `url("/images/work/${props.src}")` }}>
         <span className="bg-white p-2 rounded-lg">{props.title}</span>
       </a>
-      {/* <LiveSite href={props.liveSite} /> */}
     </div>
   );
 }
