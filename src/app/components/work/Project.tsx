f98bbfc7b0a318281f699efa04b73d860e9b0810
SharanKarchella <sharankarchella@gmail.com>
2024-12-12 16:14:54 +0530

    full_updated

--- src/app/components/work/Project.tsx
+++ src/app/components/work/Project.tsx
@@ -3,11 +3,9 @@ import React from "react";
 
 function Project(props: any) {
   return (
-    <div>
       <a href={props.link} className="h-60 bg-cyan-600 p-4 rounded-xl bg-cover bg-right" style={{ backgroundImage: `url("/images/work/${props.src}")` }}>
         <span className="bg-white p-2 rounded-lg">{props.title}</span>
       </a>
-    </div>
   );
 }
 export default Project;
--- src/app/components/work/Projects.tsx
+++ src/app/components/work/Projects.tsx
@@ -4,11 +4,13 @@ import Project from './Project'
 function Projects() {
   return (
     <div className='grid grid-cols-3 gap-4'>
-      <Project link="https://github.com/SharanKarchella/blog" title="Medium-Website-Blog" src="medium.png" />
-      <Project link="https://github.com/SharanKarchella/Disney-Hotstar" title="Disney-Hotstar-Website" src="dh.png" />
-      <Project link="https://github.com/SharanKarchella/git-prof-finder" title="git-profile-finder" src="git.png" />
+      <Project link="https://blog-bice-rho-26.vercel.app/signup" title="Medium-Website-Blog" src="medium.png" />
+      <Project link="https://movie-stream-movies.netlify.app/" title="Disney-Hotstar-Website" src="dh.png" />
+      <Project link="https://my-portfolio-nextjs-tailwind.netlify.app/" title="Portfolio-Website" src="portfolio.png" />
       <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car.png" />
       <Project link="https://github.com/SharanKarchella/car-website" title="Car-Website" src="car3.png" />
+      <Project link="https://github.com/SharanKarchella/git-prof-finder" title="git-profile-finder" src="git.png" />
+
 
     </div>
   )
